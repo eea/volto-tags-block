bb353ce424ab8713225568848789cbdc57c13800
Maurice Reim <reim@interaktiv.de>
2025-01-13 13:21:50 +0100

    Make Tag Block schema and labels translatable

--- src/TagsBlock/schema.js
+++ src/TagsBlock/schema.js
@@ -1,9 +1,46 @@
-const tagSchema = {
-  title: 'Tag item',
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  tagItem: {
+    id: 'Tag item',
+    defaultMessage: 'Tag item',
+  },
+  default: {
+    id: 'Default',
+    defaultMessage: 'Default',
+  },
+  openLinkInNewTab: {
+    id: 'openLinkInNewTab',
+    defaultMessage: 'Open link in new tab?',
+  },
+  tagBlock: {
+    id: 'tagBlock',
+    defaultMessage: 'Tag block',
+  },
+  title: {
+    id: 'title',
+    defaultMessage: 'Title',
+  },
+  alignment: {
+    id: 'alignment',
+    defaultMessage: 'Alignment',
+  },
+  showTagIcon: {
+    id: 'showTagIcon',
+    defaultMessage: 'Enable tag items icon',
+  },
+  tagItems: {
+    id: 'tagItems',
+    defaultMessage: 'Tag items',
+  }
+})
+
+const tagSchema = (intl) => ({
+  title: intl.formatMessage(messages.tagItem),
   fieldsets: [
     {
       id: 'default',
-      title: 'Default',
+      title: intl.formatMessage(messages.default),
       fields: ['category', 'href', 'openLinkInNewTab'],
     },
   ],
@@ -17,45 +54,45 @@ const tagSchema = {
       widget: 'url',
     },
     openLinkInNewTab: {
-      title: 'Open link in new tab',
+      title: intl.formatMessage(messages.openLinkInNewTab),
       type: 'boolean',
     },
   },
   required: [],
-};
+});
 
-const schema = {
-  title: 'Tag block',
+const schema = (intl) => ({
+  title: intl.formatMessage(messages.tagBlock),
 
   fieldsets: [
     {
       id: 'default',
-      title: 'Default',
+      title: intl.formatMessage(messages.default),
       fields: ['title', 'position', 'showTagIcon', 'items'],
     },
   ],
 
   properties: {
     title: {
-      title: 'Title',
+      title: intl.formatMessage(messages.title),
       widget: 'textarea',
     },
     position: {
-      title: 'Alignment',
+      title: intl.formatMessage(messages.alignment),
       widget: 'align',
     },
     showTagIcon: {
-      title: 'Enable tag items icon',
+      title: intl.formatMessage(messages.showTagIcon),
       type: 'boolean',
     },
     items: {
-      title: 'Tag items',
+      title: intl.formatMessage(messages.tagItems),
       widget: 'object_list',
-      schema: tagSchema,
+      schema: tagSchema(intl),
     },
   },
 
   required: [],
-};
+});
 
 export default schema;
